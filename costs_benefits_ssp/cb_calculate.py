4b9bb93030804cc9a771313cfe2821fc8eec633b
Hermilo Cortes <hermilocg@hotmail.com>
2025-01-29 12:20:56 -0600

    fix sanitation cost

--- costs_benefits_ssp/cb_calculate.py
+++ costs_benefits_ssp/cb_calculate.py
@@ -159,13 +159,17 @@ class CostBenefits:
         all_tx_on_ssp_data = list(data.strategy_code.unique())
         all_tx_on_ssp_data.remove(self.strategy_code_base)
 
+
         data_strategy = self.cb_get_data_from_wide_to_long(data, all_tx_on_ssp_data, sanitation_classification["variable"].to_list())
         data_strategy = data_strategy.merge(right = sanitation_classification, on='variable')
 
 
         population = self.cb_get_data_from_wide_to_long(data, all_tx_on_ssp_data, ['population_gnrl_rural', 'population_gnrl_urban'])
-        data_strategy = data_strategy.merge(right = population, on = ['region', 'time_period', 'future_id'], suffixes = ["", ".pop"])
-        data_strategy = data_strategy[data_strategy["population_variable"].isin(data_strategy["variable.pop"])].reset_index()
+        population = population.rename(columns = {"variable" : "population_variable"})
+
+
+        data_strategy = data_strategy.merge(right = population[ ["strategy_code", "region", "time_period", "future_id", "population_variable", "value"]], on = ["strategy_code", "region", "time_period", "future_id", "population_variable"], suffixes = ["", ".pop"])
+        data_strategy = data_strategy[data_strategy["population_variable"].isin(data_strategy["population_variable"])].reset_index(drop=True)
         data_strategy["pop_in_pathway"] = data_strategy["value"]*data_strategy["value.pop"]
 
 
@@ -173,28 +177,34 @@ class CostBenefits:
         data_base = self.cb_get_data_from_wide_to_long(data, self.strategy_code_base, sanitation_classification["variable"].to_list())
         data_base = data_base.merge(right = sanitation_classification, on = 'variable')
 
-    
-        population_base = self.cb_get_data_from_wide_to_long(data, self.strategy_code_base, ['population_gnrl_rural', 'population_gnrl_urban'])
-    
-        data_base = data_base.merge(right = population_base, on = ['region', 'time_period', 'future_id'], suffixes = ["", ".pop"])
 
-        data_base = data_base[data_base["population_variable"].isin(data_base["variable.pop"])].reset_index(drop = True)
+        population_base = self.cb_get_data_from_wide_to_long(data, self.strategy_code_base, ['population_gnrl_rural', 'population_gnrl_urban'])
+        population_base = population_base.rename(columns = {"variable" : "population_variable"})
+
+        data_base = data_base.merge(right = population_base[['region', 'time_period', 'future_id', "population_variable", "value"]], on = ['region', 'time_period', 'future_id', "population_variable"], suffixes = ["", ".pop"])
+
+        data_base = data_base[data_base["population_variable"].isin(data_base["population_variable"])].reset_index(drop = True)
         data_base["pop_in_pathway"] = data_base["value"]*data_base["value.pop"]
-    
+
+
+        data_strategy.merge(right=data_base[['region', 'time_period', 'future_id', 'pop_in_pathway']],  on = ['region', 'time_period', 'future_id'], suffixes = ["", "_base_strat"])
+
+
+
         data_new = pd.concat([data_strategy, data_base], ignore_index = True)
-    
+
         #reduce it by the sanitation category
 
         gp_vars = ["primary_id", "region", "time_period", "strategy_code", "future_id", "difference_variable"]
 
         data_new_summarized = data_new.groupby(gp_vars).agg({"pop_in_pathway" : "sum"}).rename(columns = {"pop_in_pathway" : "value"}).reset_index()
         data_new_summarized = data_new_summarized.rename(columns = {"difference_variable" : "variable"})
-    
+
         new_list_of_variables = data_new_summarized["variable"].unique()  
-        
+
         pivot_index_vars = [i for i in data_new_summarized.columns if i not in ["variable", "value"]]
         data_new_summarized_wide = data_new_summarized.pivot(index = pivot_index_vars, columns="variable", values="value").reset_index()  
-        
+
         data = data.merge(right=data_new_summarized_wide, on = ['primary_id', 'region', 'time_period', 'future_id', 'strategy_code'])
 
         return data
